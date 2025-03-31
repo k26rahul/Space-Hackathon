f7c290b2c2317706eeb4acd0340078b1a56e1930
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-04-01 02:23:53 +0530

    WIP

--- learning-3js/3d-container/controls.js
+++ learning-3js/3d-container/controls.js
@@ -1,39 +1,100 @@
 import { GUI } from 'dat.gui';
-import * as THREE from 'three';
 
 const gui = new GUI();
 
+const posToDim = {
+  x: 'width',
+  y: 'height',
+  z: 'depth',
+};
+
 export function setupControls({ container, items }) {
-  // Create a parent folder for Container
+  // Store controller references
+  const containerPosControllers = {}; // { x: controller, y: controller, ... }
+  const itemsControllers = []; // each { item, dimControllers: [{dim, ctrl}], posControllers: [{dim, ctrl}] }
+
+  function getContainerPosRange(dim) {
+    const val = container.dimensions[posToDim[dim]] || 100;
+    return { min: -val, max: val };
+  }
+
+  function getItemPosRange(item, dim) {
+    if (dim === 'x') return { min: 0, max: container.dimensions.width - item.size.width };
+    if (dim === 'y') return { min: 0, max: container.dimensions.height - item.size.height };
+    if (dim === 'z') return { min: -(container.dimensions.depth - item.size.depth), max: 0 };
+  }
+
+  function updateItemSizeRanges(item, dimControllers) {
+    dimControllers.forEach(({ dim, ctrl }) => {
+      ctrl.min(1).max(container.dimensions[dim] || 100);
+    });
+  }
+
+  function updateItemPosRanges(item, posControllers) {
+    posControllers.forEach(({ dim, ctrl }) => {
+      const { min, max } = getItemPosRange(item, dim);
+      ctrl.min(min).max(max);
+    });
+  }
+
+  // Container Folder
   const containerFolder = gui.addFolder('Container');
 
-  // Dimensions folder
+  // Dimensions
   const containerDimFolder = containerFolder.addFolder('Dimensions');
   Object.keys(container.dimensions).forEach(dim => {
-    containerDimFolder.add(container.dimensions, dim, -100, 300).onChange(value => {
+    containerDimFolder.add(container.dimensions, dim, 0, 300).onChange(value => {
       container.updateDimensions();
+      // update container position sliders when container size changes
+      Object.keys(container.position).forEach(dim => {
+        const { min, max } = getContainerPosRange(dim);
+        containerPosControllers[dim].min(min).max(max);
+      });
+      // refresh all item sliders when container size changes
+      itemsControllers.forEach(({ item, dimControllers, posControllers }) => {
+        updateItemSizeRanges(item, dimControllers);
+        updateItemPosRanges(item, posControllers);
+      });
     });
   });
 
-  // Position folder
+  // Position (DRY: use getContainerPosRange)
   const containerPosFolder = containerFolder.addFolder('Position');
   Object.keys(container.position).forEach(dim => {
-    containerPosFolder.add(container.position, dim, -100, 100).onChange(value => {
+    const { min, max } = getContainerPosRange(dim);
+    const ctrl = containerPosFolder.add(container.position, dim, min, max).onChange(value => {
       container.updatePosition();
     });
+    containerPosControllers[dim] = ctrl;
   });
 
+  // Items
   items.forEach(item => {
     const itemFolder = gui.addFolder(item.name);
-    const posFolder = itemFolder.addFolder('Position');
     const dimFolder = itemFolder.addFolder('Dimensions');
+    const posFolder = itemFolder.addFolder('Position');
+    const dimControllers = [];
+    const posControllers = [];
 
-    Object.keys(item.position).forEach(dim => {
-      posFolder.add(item.position, dim, -100, 100).onChange(value => item.updatePosition());
-    });
-
+    // Dimensions
     Object.keys(item.size).forEach(dim => {
-      dimFolder.add(item.size, dim, -100, 100).onChange(value => item.updateSize());
+      const max = container.dimensions[dim] || 100;
+      const ctrl = dimFolder.add(item.size, dim, 1, max).onChange(value => {
+        item.updateSize();
+        updateItemPosRanges(item, posControllers);
+      });
+      dimControllers.push({ dim, ctrl });
     });
+
+    // Position (DRY: use getItemPosRange)
+    Object.keys(item.position).forEach(dim => {
+      const { min, max } = getItemPosRange(item, dim);
+      const ctrl = posFolder.add(item.position, dim, min, max).onChange(value => {
+        item.updatePosition();
+      });
+      posControllers.push({ dim, ctrl });
+    });
+
+    itemsControllers.push({ item, dimControllers, posControllers });
   });
 }
