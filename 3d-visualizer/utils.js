9ab6709088b0acdbc5ce3d241b07b859ca0296fb
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-04-02 18:24:47 +0530

    📦

--- 3d-visualizer/utils.js
+++ 3d-visualizer/utils.js
@@ -1,10 +1,3 @@
-export function hexToRgb(hex) {
-  const r = (hex >> 16) & 255;
-  const g = (hex >> 8) & 255;
-  const b = hex & 255;
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
 export const axisToDimension = {
   x: 'width',
   y: 'height',
